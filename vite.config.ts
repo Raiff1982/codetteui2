21de4b74b33b30056bbd1b720c7d8892175e08f9
Jonathan Harrison <145727918+Raiff1982@users.noreply.github.com>
2025-09-02 15:25:09 -0500

    Fix mixed content error by configuring Vite for HTTP

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   root: '.',
   plugins: [react()],
+  server: {
+    https: false
+  },
   build: {
     rollupOptions: {
       input: 'index.html'
